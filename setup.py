6a92f11e6eb5703837a9acd9d0c4f9f0076ff2da
Alexey Kotlyarov <alexey@infoxchange.net.au>
2014-05-06 09:05:33 +1000

    Version bump

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ assert version_info >= (3,), "Python 3 is required."
 
 setup(
     name='docker-forklift',
-    version='0.2.13',
+    version='0.2.14',
     description='Utility for running a container',
     author='Infoxchange Australia development team',
     author_email='devs@infoxchange.net.au',
