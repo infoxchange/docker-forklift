ba54984ac7783c96d0465cb1784fe73aec14713c
Alexey Kotlyarov <alexey@infoxchange.net.au>
2014-11-05 13:44:33 +1100

    Version bump

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open('requirements.txt') as requirements, \
         open('test_requirements.txt') as test_requirements:
     setup(
         name='docker-forklift',
-        version='0.2.34',
+        version='0.2.35',
         description='Utility for running a container',
         author='Infoxchange Australia development team',
         author_email='devs@infoxchange.net.au',
