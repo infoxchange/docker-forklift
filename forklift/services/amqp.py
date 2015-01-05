1e89f112abbf9e34aa80308ab66f3225b11dae05
Danielle Madeley <dmadeley@infoxchange.net.au>
2015-01-05 15:36:43 +1100

    Update copyright

--- forklift/services/amqp.py
+++ forklift/services/amqp.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2014  Infoxchange Australia
+# Copyright 2015  Infoxchange
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
