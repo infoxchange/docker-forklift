a000b069fc5c48c45eb4b3737406f38147ecc0df
Kane Cavanagh <kcavanagh@infoxchange.org>
2015-07-30 10:59:37 +1000

    Bump version to 0.2.45

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open('requirements.txt') as requirements, \
         open('test_requirements.txt') as test_requirements:
     setup(
         name='docker-forklift',
-        version='0.2.44',
+        version='0.2.45',
         description='Utility for running a container',
         author='Infoxchange Australia development team',
         author_email='devs@infoxchange.net.au',
